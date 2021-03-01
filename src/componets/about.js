d7821fe2eacb27f4ce467b0a087197817c6a50fe
JuukoHenry12 <juukohenry23@gmail.com>
2021-03-01 14:02:39 +0200

    meet  commit

--- src/componets/about.js
+++ src/componets/about.js
@@ -12,7 +12,7 @@ const About = () => {
             <Col md={12}>
               <iframe
                 width="100%"
-                height="350"
+                height="450"
                 src="https://www.youtube.com/embed/Q0azZ7n696Y"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
